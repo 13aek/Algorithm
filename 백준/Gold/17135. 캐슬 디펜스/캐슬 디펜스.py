9eb81b98c867db9872fc93d2ed3d76277997b5eb
13aek <baekus2209@naver.com>
2025-08-22 12:20:07 +0900

    [Gold III] Title: 캐슬 디펜스, Time: 436 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/17135. 캐슬 디펜스/캐슬 디펜스.py
+++ 백준/Gold/17135. 캐슬 디펜스/캐슬 디펜스.py
@@ -1,6 +1,7 @@
 import sys
 import itertools
-from copy import deepcopy
+
+# sys.stdin = open('input.txt')
 
 
 # 두 격자판의 거리를 계산하는 함수
@@ -28,26 +29,30 @@ def end_turn(arr):
 # 궁수가 해당 턴에 적을 공격해서 제거한 수를 반환하는 함수
 # 한 턴에 동시 공격이 가능함..
 def attack_enemy(arr, d):
-    enemy_position = sorted(find_enemy(arr))[::-1]
-    attack_enemy_positions = []
+    enemy_position = find_enemy(arr)    # 변경
+    targets = set()  # (r, c) 튜플로 중복 방지
+
     for c in range(M):
         if arr[N][c] == 2:
             x, y = -1, -1
             min_dis = float("inf")
-            for i in enemy_position:
-                if distance(N, c, i[0], i[1]) < min_dis and distance(N, c, i[0], i[1]) <= d:
-                    min_dis = distance(N, c, i[0], i[1])
-                    x, y = i[0], i[1]
-                elif distance(N, c, i[0], i[1]) == min_dis and distance(N, c, i[0], i[1]) <= d:
-                    if y > i[1]:
-                        x, y = i[0], i[1]
-            if (x >= 0 and y >= 0) and [x, y] not in attack_enemy_positions:
-                attack_enemy_positions += [[x, y]]
+            for er, ec in enemy_position:
+                dist = distance(N, c, er, ec)
+                if dist <= d:
+                    if dist < min_dis or (dist == min_dis and ec < y):
+                        min_dis = dist
+                        x, y = er, ec
 
-    for position in attack_enemy_positions:
-        arr[position[0]][position[1]] = 0
+            if x >= 0:
+                targets.add((x, y))
 
-    return arr
+    killed = 0
+    for r, c in targets:
+        if arr[r][c] == 1:
+            arr[r][c] = 0
+            killed += 1
+
+    return arr, killed
 
 
 # 궁수를 성벽에 배치하는 함수
@@ -59,19 +64,14 @@ def place_archer(arr, c):
 
 # 디펜스 게임이 종료 될때까지 진행하는 함수
 def play(arr, c):
-    n = len(find_enemy(arr))
-    eliminate_enemy_count = 0
-    attack = attack_enemy(arr, D)
-    eliminate_enemy_count += (n - len(find_enemy(attack)))
+    if not find_enemy(arr):
+        return 0
+
+    attack, killed = attack_enemy(arr, D)
     next_turn = end_turn(attack)
 
-    if len(find_enemy(next_turn)):
-        return eliminate_enemy_count + play(next_turn, c)
+    return killed + play(next_turn, c)
 
-    return eliminate_enemy_count
-
-
-# sys.stdin = open('input.txt')
 
 N, M, D = map(int, sys.stdin.readline().strip().split())
 grid = [list(map(int, sys.stdin.readline().strip().split())) for _ in range(N)]
@@ -79,11 +79,9 @@ castle = [[3] * M]
 
 enemy_count = len(find_enemy(grid))
 
-combs = list(itertools.combinations(list(range(M)), 3))
-
 max_eliminate_enemy_count = 0
-for comb in combs:
-    total_grid = deepcopy(grid) + deepcopy(castle)
+for comb in itertools.combinations(list(range(M)), 3):
+    total_grid = [row[:] for row in grid] + [castle[0][:]]
     game_grid = place_archer(total_grid, comb)
     cnt = play(game_grid, comb)
     max_eliminate_enemy_count = max(max_eliminate_enemy_count, cnt)
