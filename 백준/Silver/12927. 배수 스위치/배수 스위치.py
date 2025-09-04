535ce6cacd6914f68f8e2881e31d67c1f4d3af8a
13aek <baekus2209@naver.com>
2025-09-04 16:40:05 +0900

    [Silver IV] Title: 배수 스위치, Time: 68 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/12927. 배수 스위치/배수 스위치.py
+++ 백준/Silver/12927. 배수 스위치/배수 스위치.py
@@ -4,16 +4,21 @@ bulb = list(sys.stdin.readline().strip())
 
 cnt = 0
 for i in range(len(bulb)):
+    # 현재 전구가 켜져 있으면
     if bulb[i] == 'Y':
-        for j in range(len(bulb)):
+        # 현재 위치부터 배수를 돌면서 전구의 상태를 반전시킴
+        for j in range(i, len(bulb)):
             if (j + 1) % (i + 1) == 0:
                 if bulb[j] == 'Y':
                     bulb[j] = 'N'
                 else:
                     bulb[j] = 'Y'
+        # 횟수 + 1
         cnt += 1
 
+# 전구의 상태가 켜진게 있으면
 if 'Y' in bulb:
+    # -1
     cnt = -1
 
 print(cnt)
