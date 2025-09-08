5a574a82238774fa44301a865fb4b929f53a7553
13aek <baekus2209@naver.com>
2025-09-08 19:08:08 +0900

    [Gold II] Title: 집 구하기, Time: 1152 ms, Memory: 110872 KB -BaekjoonHub

---
+++ 백준/Gold/13911. 집 구하기/집 구하기.py
@@ -0,0 +1,62 @@
+import sys
+import heapq
+
+
+def dijkstra(start_nodes):
+    dist = [float('inf')] * (V + 1)
+    pq = []
+
+    for s in start_nodes:
+        if dist[s] > 0:
+            dist[s] = 0
+            heapq.heappush(pq, (0, s))
+
+    while pq:
+        d, u = heapq.heappop(pq)
+
+        if d > dist[u]:
+            continue
+
+        for v, w in adj_list[u]:
+            nd = d + w
+            if nd < dist[v]:
+                dist[v] = nd
+                heapq.heappush(pq, (nd, v))
+
+    return dist
+
+
+line = sys.stdin.readline
+
+V, E = map(int, line().split())
+
+adj_list = [[] for _ in range(V + 1)]
+for i in range(E):
+    u, v, w = map(int, map(int, line().split()))
+    adj_list[u].append((v, w))
+    adj_list[v].append((u, w))
+
+M, x = map(int, line().split())
+m_v = list(map(int, line().split()))
+S, y = map(int, line().split())
+s_v = list(map(int, line().split()))
+
+is_store = [False] * (V + 1)
+for a in m_v:
+    is_store[a] = True
+for b in s_v:
+    is_store[b] = True
+
+dm = dijkstra(m_v)
+ds = dijkstra(s_v)
+
+ans = float('inf')
+for v in range(1, V+1):
+    if is_store[v]:
+        continue
+    if dm[v] <= x and ds[v] <= y:
+        s = dm[v] + ds[v]
+        if s < ans:
+            ans = s
+
+print(-1 if ans == float('inf') else ans)
