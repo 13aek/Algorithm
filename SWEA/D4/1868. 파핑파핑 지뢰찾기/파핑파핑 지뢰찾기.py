5276db09c95d91f364008af37ee240805bcfa6d2
13aek <baekus2209@naver.com>
2025-09-09 14:17:44 +0900

    [D4] Title: 파핑파핑 지뢰찾기, Time: 3,719 ms, Memory: 167,464 KB -BaekjoonHub

--- SWEA/D4/1868. 파핑파핑 지뢰찾기/파핑파핑 지뢰찾기.py
+++ SWEA/D4/1868. 파핑파핑 지뢰찾기/파핑파핑 지뢰찾기.py
@@ -5,37 +5,22 @@ dr = [-1, -1, 0, 1, 1, 1, 0, -1]
 dc = [0, 1, 1, 1, 0, -1, -1, -1]
 
 
-def bfs(start_row, start_col, arr):
+def bfs(start_row, start_col):
     q = deque()
     q.append((start_row, start_col))
-    visited = set()
     while q:
-        cnt = 0
-        candidates = []
         x, y = q.popleft()
 
-        if (x, y) in visited:
-            continue
-        else:
-            visited.add((x, y))
+        if adj[x][y] == 0:
+            for i in range(8):
+                nr = x + dr[i]
+                nc = y + dc[i]
 
-        for i in range(8):
-            nr = x + dr[i]
-            nc = y + dc[i]
-
-            if 0 <= nr < N and 0 <= nc < N:
-                if arr[nr][nc] == '*':
-                    cnt = 1
-                    break
-                elif arr[nr][nc] == '.':
-                    candidates.append((nr, nc))
-
-        if cnt == 0:
-            for a, b in candidates:
-                if (a, b) not in visited:
-                    q.append((a, b))
-
-    return len(visited), visited
+                if 0 <= nr < N and 0 <= nc < N:
+                    if adj[nr][nc] != -1 and not visited[nr][nc]:
+                        visited[nr][nc] = True
+                        if adj[nr][nc] == 0:
+                            q.append((nr, nc))
 
 
 T = int(input())
@@ -43,34 +28,37 @@ T = int(input())
 for tc in range(1, T+1):
     N = int(input())
     string = [list(map(str, input().strip())) for _ in range(N)]
-    seen = [[0] * N for _ in range(N)]
-    result = 1
+    result = 0
 
-    click_candidates = []
+    adj = [[0] * N for _ in range(N)]
     for r in range(N):
         for c in range(N):
-            if string[r][c] != '*':
-                is_star = False
-                for i in range(8):
-                    nr = r + dr[i]
-                    nc = c + dc[i]
+            if string[r][c] == '*':
+                adj[r][c] = -1
+                continue
 
-                    if 0 <= nr < N and 0 <= nc < N:
-                        if string[nr][nc] == '*':
-                            is_star = True
-                            break
-                if is_star:
-                    click_candidates.append((1, r, c, [(r, c)]))
-                else:
-                    count, visit = bfs(r, c, string)
-                    click_candidates.append((count, r, c, visit))
+            cnt = 0
+            for i in range(8):
+                nr = r + dr[i]
+                nc = c + dc[i]
 
-    click_candidates.sort(reverse=True, key=lambda x: x[0])
+                if 0 <= nr < N and 0 <= nc < N:
+                    if string[nr][nc] == '*':
+                        cnt += 1
+            adj[r][c] = cnt
 
-    is_clicked = set(click_candidates[0][3])
-    for _, a, b, v in click_candidates[1:]:
-        if (a, b) not in is_clicked:
-            is_clicked |= set(v)
-            result += 1
+    visited = [[False] * N for _ in range(N)]
+    for r in range(N):
+        for c in range(N):
+            if adj[r][c] == 0 and not visited[r][c]:
+                result += 1
+                visited[r][c] = True
+                bfs(r, c)
+
+    for r in range(N):
+        for c in range(N):
+            if adj[r][c] >= 0 and not visited[r][c]:
+                result += 1
 
     print(f"#{tc} {result}")
+
