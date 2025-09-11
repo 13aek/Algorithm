7ec2c0f4b8e208f525366dbefc03bf82f5d92668
13aek <baekus2209@naver.com>
2025-09-11 12:32:05 +0900

    [D3] Title: [파이썬 S/W 문제해결 구현] 5일차 - 최소 생산 비용, Time: 427 ms, Memory: 62,848 KB -BaekjoonHub

--- SWEA/D3/5209. ［파이썬 S／W 문제해결 구현］ 5일차 － 최소 생산 비용/［파이썬 S／W 문제해결 구현］ 5일차 － 최소 생산 비용.py
+++ SWEA/D3/5209. ［파이썬 S／W 문제해결 구현］ 5일차 － 최소 생산 비용/［파이썬 S／W 문제해결 구현］ 5일차 － 최소 생산 비용.py
@@ -1,13 +1,11 @@
 def backtrack(arr, cur_r, cur_c, cur_cost):
     global min_cost
 
-    n_cost = cur_cost + arr[cur_r][cur_c]
-
     # 종료 조건
     # 현재 행이 N 과 똑같아졌을때
     if cur_r == N - 1:
-        if min_cost > n_cost:
-            min_cost = n_cost
+        if min_cost > cur_cost:
+            min_cost = cur_cost
         return
 
     # 조기 종료 조건
@@ -23,6 +21,7 @@ def backtrack(arr, cur_r, cur_c, cur_cost):
 
     for nc in range(N):
         if not visited[nc]:
+            n_cost = cur_cost + arr[nr][nc]
             backtrack(arr, nr, nc, n_cost)
             visited[nc] = False
 
@@ -37,6 +36,6 @@ for tc in range(1, T+1):
 
     for c in range(N):
         visited = [False] * N
-        backtrack(Vij, 0, c, 0)
+        backtrack(Vij, 0, c, Vij[0][c])
 
     print(f"#{tc} {min_cost}")
