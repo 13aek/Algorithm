ec4849bf1ad45d13fd78b15c650a3e01f6207843
13aek <baekus2209@naver.com>
2025-09-27 12:17:02 +0900

    [D4] Title: [파이썬 S/W 문제해결 구현] 6일차 - 연산, Time: 269 ms, Memory: 93,752 KB -BaekjoonHub

--- SWEA/D4/5247. ［파이썬 S／W 문제해결 구현］ 6일차 － 연산/［파이썬 S／W 문제해결 구현］ 6일차 － 연산.py
+++ SWEA/D4/5247. ［파이썬 S／W 문제해결 구현］ 6일차 － 연산/［파이썬 S／W 문제해결 구현］ 6일차 － 연산.py
@@ -2,26 +2,23 @@ from collections import deque
 
 
 def bfs(num):
-    global result
-    q = deque([(num, 0)])
-    visited = [False] * 1000001
-    visited[num] = True
+    q = deque([num])
+    board[N] = 0
 
     while q:
-        cur_num, cnt = q.popleft()
+        cur_num = q.popleft()
 
         if cur_num == M:
-            result = cnt
-            return
+            break
 
-        for next_num in [cur_num + 1, cur_num - 1, cur_num * 2, cur_num - 10]:
-            if 1 <= next_num <= 1000000 and not visited[next_num]:
-                visited[next_num] = True
-                q.append((next_num, cnt + 1))
+        for next_num in (cur_num + 1, cur_num - 1, cur_num * 2, cur_num - 10):
+            if 1 <= next_num <= 1000000 and board[next_num] == -1:
+                q.append(next_num)
+                board[next_num] = board[cur_num] + 1
 
 
 for tc in range(1, int(input()) + 1):
     N, M = map(int, input().split())
-    result = 0
+    board = [-1] * 1000001
     bfs(N)
-    print(f"#{tc} {result}")
+    print(f"#{tc} {board[M]}")
