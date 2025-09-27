57b252f8465ae448e21dae10854512aaa1f20674
13aek <baekus2209@naver.com>
2025-09-27 11:59:57 +0900

    [D4] Title: [파이썬 S/W 문제해결 구현] 6일차 - 연산, Time: 510 ms, Memory: 100,084 KB -BaekjoonHub

--- SWEA/D4/5247. ［파이썬 S／W 문제해결 구현］ 6일차 － 연산/［파이썬 S／W 문제해결 구현］ 6일차 － 연산.py
+++ SWEA/D4/5247. ［파이썬 S／W 문제해결 구현］ 6일차 － 연산/［파이썬 S／W 문제해결 구현］ 6일차 － 연산.py
@@ -4,7 +4,8 @@ from collections import deque
 def bfs(num):
     global result
     q = deque([(num, 0)])
-    visited = set([num])
+    visited = [False] * 1000001
+    visited[num] = True
 
     while q:
         cur_num, cnt = q.popleft()
@@ -14,8 +15,8 @@ def bfs(num):
             return
 
         for next_num in [cur_num + 1, cur_num - 1, cur_num * 2, cur_num - 10]:
-            if 1 <= next_num <= 1000000 and next_num not in visited:
-                visited.add(next_num)
+            if 1 <= next_num <= 1000000 and not visited[next_num]:
+                visited[next_num] = True
                 q.append((next_num, cnt + 1))
 
 
