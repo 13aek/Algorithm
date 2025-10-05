4c5d528a36de11c22bd076456d78909687cf97b6
13aek <baekus2209@naver.com>
2025-10-05 16:27:10 +0900

    [Unrated] Title: [모의 SW 역량테스트] 차량 정비소, Time: 286 ms, Memory: 68,352 KB -BaekjoonHub

--- SWEA/Unrated/2477. ［모의 SW 역량테스트］ 차량 정비소/［모의 SW 역량테스트］ 차량 정비소.py
+++ SWEA/Unrated/2477. ［모의 SW 역량테스트］ 차량 정비소/［모의 SW 역량테스트］ 차량 정비소.py
@@ -1,52 +1,60 @@
+from heapq import heapify, heappop, heappush
+
+
+def bfs():
+    ans = 0
+    reception = [-1] * (N + 1)
+    repair = [-1] * (M + 1)
+    waiting_reception = []
+    for idx, value in enumerate(tk):
+        # (tk, 고객번호, 접수창구방문여부, 수리창구방문여부, 방문한 접수창구, 방문한 수리 창구)
+        heappush(waiting_reception, (value, idx + 1, 0, 0))
+    waiting_repair = []
+
+    while waiting_reception:
+        cur_time, customer, is_reception, is_repair = heappop(waiting_reception)
+
+        # 1. 아직 접수창구에 방문하지 않았으면
+        # 1.1 접수 창구에 자리가 있으면
+        if any(reception[i] < cur_time for i in range(1, N+1)):
+            for i in range(1, N + 1):
+                if reception[i] < cur_time:
+                    reception[i] = cur_time + reception_desk[i] - 1
+                    heappush(waiting_repair, (reception[i] + 1, i, customer))
+                    break
+        # 1.2 접수 창구에 자리가 없으면
+        else:
+            idx = min(range(1, N+1), key=lambda x: reception[x])
+            reception[idx] += reception_desk[idx]
+            heappush(waiting_repair, (reception[idx] + 1, idx, customer))
+
+    while waiting_repair:
+        cur_time, rcp_num, customer = heappop(waiting_repair)
+
+        # 2. 아직 수리 창구에 방문하지 않았으면
+        # 2.1 수리 창구에 자리가 있으면
+        if any(repair[i] < cur_time for i in range(1, M+1)):
+            for i in range(1, M + 1):
+                if repair[i] < cur_time:
+                    repair[i] = cur_time + repair_desk[i] - 1
+                    if (rcp_num, i) == (A, B):
+                        ans += customer
+                    break
+        # 2.2 수리 창구에 자리가 없으면
+        else:
+            idx = min(range(1, M+1), key=lambda x: repair[x])
+            repair[idx] += repair_desk[idx]
+            if (rcp_num, idx) == (A, B):
+                ans += customer
+
+    return -1 if ans == 0 else ans
+
+
 for tc in range(1, int(input()) + 1):
     N, M, K, A, B = map(int, input().split())
-    reception_desk = list(map(int, input().split()))
-    repair_desk = list(map(int, input().split()))
+    reception_desk = [-1] + list(map(int, input().split()))
+    repair_desk = [-1] + list(map(int, input().split()))
     tk = list(map(int, input().split()))
 
-    ans = 0
-    waiting_repair = []
-    reception = [-1] * N
-    repair = [-1] * M
-    reception_num = []
-    repair_num = [0] * K
-
-    for customer in range(K):
-        check = 0
-
-        for n in range(N):
-            if reception[n] < tk[customer]:
-                reception[n] = tk[customer] + reception_desk[n] - 1
-                reception_num.append(n)
-                check = 1
-                waiting_repair.append((reception[n] + 1, n, customer))
-                break
-
-        if check == 0:
-            idx = reception.index(min(reception))
-            reception_num.append(idx)
-            reception[idx] += reception_desk[idx]
-            waiting_repair.append((reception[idx] + 1, idx, customer))
-
-    waiting_repair.sort()
-
-    for w in waiting_repair:
-        check = 0
-
-        for m in range(M):
-            if repair[m] < w[0]:
-                repair[m] = w[0] + repair_desk[m] - 1
-                repair_num[w[2]] = m
-                check = 1
-                break
-
-        if check == 0:
-            idx = repair.index(min(repair))
-            repair_num[w[2]] = idx
-            repair[idx] += repair_desk[idx]
-
-    for k in range(K):
-        if (reception_num[k] + 1, repair_num[k] + 1) == (A, B):
-            ans += (k + 1)
-
-    print(f"#{tc} {-1 if ans == 0 else ans}")
+    result = bfs()
+    print(f"#{tc} {result}")
