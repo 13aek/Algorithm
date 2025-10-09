8324ad13803a0bf2d1838413f785b23f927ad89a
13aek <baekus2209@naver.com>
2025-10-09 22:43:57 +0900

    [Gold IV] Title: 연구소, Time: 2620 ms, Memory: 118292 KB -BaekjoonHub

---
+++ 백준/Gold/14502. 연구소/연구소.py
@@ -0,0 +1,63 @@
+import sys
+from collections import deque
+
+line = sys.stdin.readline
+
+# 델타 방향 정의 (상하좌우)
+dr = [-1, 1, 0, 0]
+dc = [0, 0, -1, 1]
+
+
+def bfs(labor, visit):
+    q = deque(virus)
+    cnt = 0
+
+    while q:
+        cr, cc = q.popleft()
+
+        for i in range(4):
+            nr, nc = cr + dr[i], cc + dc[i]
+
+            if (0 <= nr < N and 0 <= nc < M) and labor[nr][nc] == 0:
+                labor[nr][nc] = 2
+                visit[blank.index((nr, nc))] = True
+                q.append((nr, nc))
+
+    cnt = visit.count(False)
+
+    return cnt
+
+
+def dfs(wall_count, visited, laboratory):
+    global answer
+
+    if wall_count == 3:
+        lab = [row[:] for row in laboratory]
+        visit = visited[:]
+        count = bfs(lab, visit)
+        answer = max(answer, count)
+        return
+
+    for i, (r, c) in enumerate(blank):
+        if not visited[i]:
+            visited[i] = True
+            laboratory[r][c] = 1
+            dfs(wall_count + 1, visited, laboratory)
+            laboratory[r][c] = 0
+            visited[i] = False
+
+
+N, M = map(int, line().split())
+grid = [list(map(int, line().split())) for _ in range(N)]
+virus = []
+blank = []
+for r in range(N):
+    for c in range(M):
+        if grid[r][c] == 2:
+            virus.append((r, c))
+        elif grid[r][c] == 0:
+            blank.append((r, c))
+answer = 0
+visited = [False] * len(blank)
+dfs(0, visited, grid)
+print(answer)
