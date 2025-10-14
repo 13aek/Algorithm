25603047ee03b6ce95198eb1ebdb6238e8c9734b
13aek <baekus2209@naver.com>
2025-10-14 11:43:51 +0900

    [Gold I] Title: 낚시왕, Time: 580 ms, Memory: 39136 KB -BaekjoonHub

--- 백준/Gold/17143. 낚시왕/낚시왕.py
+++ 백준/Gold/17143. 낚시왕/낚시왕.py
@@ -14,11 +14,8 @@ def simulate(shark):
     fishing = 0
     while shark:
         bucket = {}
-
-        # 1. 낚시왕 한 칸 이동
         king += 1
 
-        # 2. 낚시 시도
         candidate = False
         candi_row = float('inf')
         for key in shark.keys():
@@ -30,36 +27,58 @@ def simulate(shark):
             fishing += shark[candidate][2]
             del shark[candidate]
 
-        # 2.1 낚시킹이 가장 오른쪽열에서 낚시를 마쳤으면 중지
         if king == C:
             break
 
-        # 3. 상어 이동
-        for k, v in shark.items():
-            cr, cc = k
-            s, d, z = v
-            if d == 1 or d == 2:
-                ns = s % ((R - 1) * 2)
-            else:
-                ns = s % ((C - 1) * 2)
-            nd = d
-            nr, nc = cr, cc
-            for distance in range(1, ns + 1):
-                nr, nc = nr + drc[nd][0], nc + drc[nd][1]
+        # 2. 상어 이동 (수식 기반 O(1))
+        for (r, c), (s, d, z) in shark.items():
+            if d == 1 or d == 2:  # 상하 이동
+                cycle = (R - 1) * 2
+                move = s % cycle
+                if d == 1:
+                    nr = r - move
+                    if nr < 1:
+                        nr = 2 - nr
+                        d = 2
+                    if nr > R:
+                        nr = 2 * R - nr
+                        d = 1
+                else:
+                    nr = r + move
+                    if nr > R:
+                        nr = 2 * R - nr
+                        d = 1
+                    if nr < 1:
+                        nr = 2 - nr
+                        d = 2
+                nc = c
 
-                if nr < 1 or nr > R or nc < 1 or nc > C:
-                    nd = opposite[nd]
-                    nr, nc = nr + drc[nd][0] * 2, nc + drc[nd][1] * 2
+            else:  # 좌우 이동
+                cycle = (C - 1) * 2
+                move = s % cycle
+                if d == 4:
+                    nc = c - move
+                    if nc < 1:
+                        nc = 2 - nc
+                        d = 3
+                    if nc > C:
+                        nc = 2 * C - nc
+                        d = 4
+                else:
+                    nc = c + move
+                    if nc > C:
+                        nc = 2 * C - nc
+                        d = 4
+                    if nc < 1:
+                        nc = 2 - nc
+                        d = 3
+                nr = r
 
-            # bucket 에 다음 상어 위치 저장
-            # 3.1 상어가 같은 위치에 있는 경우
             if (nr, nc) in bucket:
-                es, ed, ez = bucket[(nr, nc)]
-                if z > ez:
-                    bucket[(nr, nc)] = [s, nd, z]
-            # 3.2 해당 위치에 아무 상어도 없을 경우
+                if z > bucket[(nr, nc)][2]:
+                    bucket[(nr, nc)] = [s, d, z]
             else:
-                bucket[(nr, nc)] = [s, nd, z]
+                bucket[(nr, nc)] = [s, d, z]
 
         shark = bucket
 
@@ -70,6 +89,6 @@ R, C, M = map(int, line().split())
 sharks = {}
 for _ in range(M):
     r, c, s, d, z = map(int, line().split())
-    sharks[(r, c)] = [s, d, z]   # 속력, 이동 방향, 크기
+    sharks[(r, c)] = [s, d, z]  # 속력, 이동 방향, 크기
 
 print(simulate(sharks))
