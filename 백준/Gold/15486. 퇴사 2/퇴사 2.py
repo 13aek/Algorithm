358831c595dd4e527135d8b91bf992b875bf397e
13aek <baekus2209@naver.com>
2025-10-20 16:12:21 +0900

    [Gold V] Title: 퇴사 2, Time: 2236 ms, Memory: 99424 KB -BaekjoonHub

--- 백준/Gold/15486. 퇴사 2/퇴사 2.py
+++ 백준/Gold/15486. 퇴사 2/퇴사 2.py
@@ -3,16 +3,14 @@ import sys
 line = sys.stdin.readline
 
 N = int(line())
-schedule = [[]] + [list(map(int, line().split())) for _ in range(N)]
-dp = [0] * (N + 2)
-max_profit = 0
+dp = [0] * (N + 1)
+
 for i in range(1, N + 1):
-    Ti = schedule[i][0]
-    Pi = schedule[i][1]
+    Ti, Pi = map(int, line().split())
 
     dp[i] = max(dp[i], dp[i-1])
 
     if i + Ti - 1 <= N:
-        dp[i + Ti] = max(dp[i + Ti], dp[i] + Pi)
+        dp[i + Ti - 1] = max(dp[i + Ti - 1], dp[i - 1] + Pi)
 
-print(max(dp))
+print(dp[N])
