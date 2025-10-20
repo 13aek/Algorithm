1f682287863a3ea1610347134ba78e71ff58f0b9
13aek <baekus2209@naver.com>
2025-10-20 09:10:52 +0900

    [Unrated] Title: [모의 SW 역량테스트] 핀볼 게임, Time: 2,003 ms, Memory: 67,072 KB -BaekjoonHub

--- SWEA/Unrated/5650. ［모의 SW 역량테스트］ 핀볼 게임/［모의 SW 역량테스트］ 핀볼 게임.py
+++ SWEA/Unrated/5650. ［모의 SW 역량테스트］ 핀볼 게임/［모의 SW 역량테스트］ 핀볼 게임.py
@@ -1,5 +1,3 @@
-from collections import deque
-
 # 델타 방향 정의 (상하좌우 우하 좌상 좌하 우상)
 drc = {0: (-1, 0), 1: (1, 0), 2: (0, -1), 3: (0, 1)}
 
@@ -18,33 +16,41 @@ opposite = {0: 1, 1: 0, 2: 3, 3: 2}
 
 def simulate(sr, sc, sd):
     cr, cc, d, s = sr, sc, sd, 0
-    start = False
 
     while True:
+        # 다음 위치
         nr, nc = cr + drc[d][0], cc + drc[d][1]
 
-        if 0 > nr or nr >= N or 0 > nc or nc >= N:
+        # 벽이나 5번 블럭을 만나면 리턴
+        if 0 > nr or nr >= N or 0 > nc or nc >= N or pinball[nr][nc] == 5:
             return s * 2 + 1
 
-        if (nr, nc) == (sr, sc) or pinball[nr][nc] == -1:
+        cell = pinball[nr][nc]
+
+        # 시작 위치로 돌아오거나 블랙홀 만나면 리턴
+        if (nr, nc) == (sr, sc) or cell == -1:
             return s
 
-        if pinball[nr][nc] == 0:
+        # 다음위치가 빈 공간이면 다음 위치로 현재위치 업데이트
+        if cell == 0:
             cr, cc, d, s = nr, nc, d, s
             continue
 
-        elif 1 <= pinball[nr][nc] <= 4:
+        # 1 ~ 4번 벽을 만나면 방향 바꾸고 현재 위치 업데이트
+        elif 1 <= cell <= 4:
             block_num = pinball[nr][nc]
             change_dir = block[block_num][d]
             cr, cc, d, s = nr, nc, change_dir, s + 1
             continue
 
-        elif pinball[nr][nc] == 5:
-            return s * 2 + 1
-
-        elif 6 <= pinball[nr][nc]:
-            nr, nc = worm_hole[(nr, nc)]
-            cr, cc, d, s = nr, nc, d, s
+        # 웜홀 만나면 연결된 웜홀 위치로 현재 위치 업데이트
+        elif 6 <= cell:
+            x1, y1 = worm_hole[cell][0]
+            x2, y2 = worm_hole[cell][1]
+            if (nr, nc) == (x1, y1):
+                cr, cc, d, s = x2, y2, d, s
+            else:
+                cr, cc, d, s = x1, y1, d, s
             continue
 
 
@@ -52,21 +58,20 @@ for tc in range(1, int(input()) + 1):
     N = int(input())
     pinball = [list(map(int, input().split())) for _ in range(N)]
 
-    worm_hole_check = {}
+    # 웜홀 위치 체크
+    worm_hole = {}
     for r in range(N):
         for c in range(N):
             if 6 <= pinball[r][c] <= 10:
-                worm_hole_check[pinball[r][c]] = worm_hole_check.get(pinball[r][c], []) + [(r, c)]
-
-    worm_hole = {}
-    for v in worm_hole_check.values():
-        worm_hole[v[0]] = v[1]
-        worm_hole[v[1]] = v[0]
+                worm_hole[pinball[r][c]] = worm_hole.get(pinball[r][c], []) + [(r, c)]
 
     answer = 0
+    # pinball map 순회
     for r in range(N):
         for c in range(N):
+            # 빈 공간일때
             if pinball[r][c] == 0:
+                # 4방향으로 탐색
                 for i in range(4):
                     answer = max(answer, simulate(r, c, i))
 
